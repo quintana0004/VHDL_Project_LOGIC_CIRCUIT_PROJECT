fd4aae68a961ceea101f4b5f57a1f1ce1db2e29f
Jessica <jessynquintana@gmail.com>
2021-09-14 14:26:00 -0400

    VHDL Template Practice Not Project

---
+++ Practice_VHDL_Template/practice_VHDL.vhd
@@ -0,0 +1,19 @@
+--This is the VHDL code 
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ent is
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        sig
+    );
+end ent;
+
+architecture rtl of ent is
+
+begin
+
+end architecture;
